da10f0b15a50322f5cc448b9e07eca1e834e8d39
llastkrakw <llastkrakw21@gmail.com>
2020-03-14 11:50:26 +0100

    register and login interfaces

---
+++ app/src/main/java/com/example/vortex/ResetPasswordThird.java
@@ -0,0 +1,14 @@
+package com.example.vortex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPasswordThird extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password_third);
+    }
+}
