09f259122868c605bd932ff518f097a6e7178291
Frank <franktsago@gmail.com>
2020-04-08 14:04:19 +0200

    paiement state part 1 OK

--- app/src/main/java/com/example/vortex/PaiementStates/PaiementState.java
+++ app/src/main/java/com/example/vortex/PaiementStates/PaiementState.java
@@ -2,14 +2,17 @@ package com.example.vortex.PaiementStates;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.example.vortex.R;
 
 public class PaiementState extends AppCompatActivity {
-
+    Button next;
     private Spinner spinner_code_phone;
     private String[] table_spinner_code ={"+237", "+224", "+221", "+91", "+1","+331"};
     @Override
@@ -19,5 +22,14 @@ public class PaiementState extends AppCompatActivity {
         spinner_code_phone = findViewById(R.id.paiementstate_spinner);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, table_spinner_code);
         spinner_code_phone.setAdapter(arrayAdapter);
+
+        next = findViewById(R.id.btn_next_paiementstate);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(PaiementState.this,PaiementState1.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/vortex/PaiementStates/PaiementState1.java
@@ -0,0 +1,16 @@
+package com.example.vortex.PaiementStates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vortex.R;
+
+public class PaiementState1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paiement_state1);
+    }
+}
